61bc017f9dfba7fe99944ca526634818d0172c11
antonkhomenko <anton.khomenko@yahoo.com>
2022-07-24 06:05:39 +0300

    final v2

--- index.js
+++ index.js
@@ -2,36 +2,40 @@ let {Builder, Key, By, until, Capabilities, logging} = require("selenium-webdriv
 let chrome = require("selenium-webdriver/chrome");
 let firefox = require("selenium-webdriver/firefox");
 let proxy = require("selenium-webdriver/proxy");
+const {ExpectedConditions} = require("protractor");
 
 
 
 
-     async function openBrowser(videoUrl, login, password) {
+async function openBrowser(videoUrl, login, password) {
+     let option = new firefox.Options()
+         .addExtensions('extension/multi_account_containers-8.0.7.xpi')
+         .addArguments('--disable-notifications')
+         .addArguments('--disable-popup-blocking');
 
-        let option = new firefox.Options()
-            .addExtensions('extension/multi_account_containers-8.0.7.xpi');
-
-        let driver = new Builder()
-            .withCapabilities(Capabilities.firefox())
-            .build();
-
-        //await signIn(driver, 'testbotantoha5@gmail.com', 'qwerty5225');
-        await signIn(driver, login, password)
-        await playVideo(driver, videoUrl);
-    }
+    let driver = await new Builder()
+        .setFirefoxOptions(option)
+        .forBrowser('firefox')
+        .build();
 
 
+    await signIn(driver, login, password, videoUrl);
 
-async function signIn(driver, login, password) {
+}
+
+
+
+async function signIn(driver, login, password, url) {
     driver.get('https://accounts.google.com/ServiceLogin/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin');
     let loginInput = await driver.findElement(By.xpath('//input[@autocomplete="username"]'));
     await loginInput.sendKeys(login, Key.RETURN);
-    await driver.sleep(1000);
+    await driver.sleep(2000);
     await driver.navigate().refresh();
     let passwordInput = await driver.findElement(By.xpath('//input[@autocomplete="current-password"]'));
-    let passwordBtn = await driver.findElement(By.xpath('//button[@class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b"]'));
+    let passwordBtn =  await driver.findElement(By.xpath('//button[@class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b"]'));
     await passwordInput.sendKeys(password);
     await passwordBtn.click();
+    await playVideo(driver, url);
 }
 
 async function playVideo(driver, videoUrl) {
@@ -41,12 +45,13 @@ async function playVideo(driver, videoUrl) {
     let videoTimer = await driver.findElement(By.xpath('//span[@class="style-scope ytd-thumbnail-overlay-time-status-renderer"]')).getText();
     let browserCloseTimer = videoTimer.split(":")[0] * 60000 + videoTimer.split(":")[1] * 1000;
     await driver.findElement(By.xpath('//*[@id="video-title"]')).click();
-    await driver.sleep(browserCloseTimer);
-    driver.close();
+    setTimeout(() => {
+        driver.close();
+    }, browserCloseTimer);
 }
 
 
-function createServer() {
+async function createServer() {
     const express = require("express");
     const path = require("path");
 
@@ -66,14 +71,15 @@ function createServer() {
 
 
     io.on('connection', (socket) => {
-        socket.on('seleniumData', (data) => {
+        socket.on('seleniumData', async (data) => {
             let [videoUrl, browserCounter, accounts] = JSON.parse(data);
             browserCounter = +browserCounter;
             let accountsArr = Object.entries(accounts);
             for(let i = 0; i < browserCounter; i++) {
-                let [login, password] = accountsArr.shift();
-                openBrowser(videoUrl, login, password);
+                let [login, password] = accountsArr[i];
+                await openBrowser(videoUrl, login, password);
             }
+            driver.close();
         })
     });
 
@@ -81,24 +87,17 @@ function createServer() {
 
     let driver = new Builder().forBrowser('chrome').build();
     driver.get("http://localhost:3000");
-
 }
 
-// let test = wrapperOpenBrowser('https://www.youtube.com/results?search_query=%5BFREE%5D+Playboi+Carti+Type+Beat++KIKI+phantom&sp=CAISBAgDEAE%253D');
-// test('mamyebali1001@gmail.com', 'suka5225');
+createServer().then((text) => console.log(text));
+
+//openBrowser('https://www.youtube.com/results?search_query=%5BFREE%5D+Playboi+Carti+Type+Beat++KIKI+phantom&sp=CAISBAgDEAE%253D', 'mamyebali1001@gmail.com', 'suka5225');
+//openBrowser('https://www.youtube.com/results?search_query=%5BFREE%5D+Playboi+Carti+Type+Beat++KIKI+phantom&sp=CAISBAgDEAE%253D', 'testbotantoha3@gmail.com ', 'qwerty5225');
+//openBrowser('https://www.youtube.com/results?search_query=%5BFREE%5D+Playboi+Carti+Type+Beat++KIKI+phantom&sp=CAISBAgDEAE%253D', 'testbotantoha4@gmail.com ', 'qwerty5225');
+//openBrowser('https://www.youtube.com/results?search_query=%5BFREE%5D+Playboi+Carti+Type+Beat++KIKI+phantom&sp=CAISBAgDEAE%253D', 'testbotantoha5@gmail.com ', 'qwerty5225');
 
 
 
-createServer();
 
-async function launch(data) {
-    let [videoUrl, browserCounter, accounts] = JSON.parse(data);
-    browserCounter = +browserCounter;
-    let accountsArr = Object.entries(accounts);
-    for(let i = 0; i < browserCounter; i++) {
-          let [login, password] = accountsArr.shift();
-        console.log(login, password);
-    }
 
-}
 
